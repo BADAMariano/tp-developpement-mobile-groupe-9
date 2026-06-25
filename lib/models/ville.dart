70ea4690cb7d5be86b4c61cdeb2c4a6857b2b2f8
BADAMariano <badamariano@gmail.com>
2026-06-25 11:57:52 +0100

    Initialisation du projet AppMeteo

---
+++ lib/models/ville.dart
@@ -0,0 +1,15 @@
+class Ville {
+  final String nom;
+  final String pays;
+  final double temperature;
+  final String condition;
+  final int humidite;
+
+  Ville({
+    required this.nom,
+    required this.pays,
+    required this.temperature,
+    required this.condition,
+    required this.humidite,
+  });
+}
