2b5c3b4d43f6e06f105d3d4c27b41e477f5b784c
BADAMariano <badamariano@gmail.com>
2026-06-26 10:32:42 +0100

    TP3 - Ajout de photoPath dans Ville

--- lib/models/ville.dart
+++ lib/models/ville.dart
@@ -4,6 +4,7 @@ class Ville {
   final double temperature;
   final String condition;
   final int humidite;
+  final String? photoPath; // NOUVEAU : chemin vers la photo
 
   Ville({
     required this.nom,
@@ -11,5 +12,18 @@ class Ville {
     required this.temperature,
     required this.condition,
     required this.humidite,
+    this.photoPath, // optionnel
   });
+
+  // Copier la ville avec une nouvelle photo
+  Ville copierAvecPhoto(String chemin) {
+    return Ville(
+      nom: nom,
+      pays: pays,
+      temperature: temperature,
+      condition: condition,
+      humidite: humidite,
+      photoPath: chemin,
+    );
+  }
 }
